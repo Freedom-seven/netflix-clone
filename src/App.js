277fb6d936af7287a2754d5331adc56a7ff09824
FREEDOM <freewhynane62@gmail.com>
2022-01-27 14:55:18 +0000

    remove unuse rows

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ function App() {
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Popular Movies" fetchUrl={requests.fetchPopular} />
+      <Row title="Upcoming Movies" fetchUrl={requests.fetchUpcoming} />
+      {/* <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> */}
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
