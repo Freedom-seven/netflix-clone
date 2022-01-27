9b9dcaa0df506f1fffa1323f17ddf9c351819263
FREEDOM <freewhynane62@gmail.com>
2022-01-27 14:56:26 +0000

    remove unuse fetch

--- src/requests.js
+++ src/requests.js
@@ -4,11 +4,13 @@ const requests = {
   fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
-  fetchActionMovies: `/disocver/movie?api_key=${APIKEY}&with_genres=28`,
-  fetchComedyMovies: `/disocver/movie?api_key=${APIKEY}&with_genres=35`,
-  fetchHorrorMovies: `/disocver/movie?api_key=${APIKEY}&with_genres=27`,
-  fetchRomanceMovies: `/disocver/movie?api_key=${APIKEY}&with_genres=10749`,
+  //   fetchActionMovies: `/movie?api_key=${APIKEY}&with_genres=28`,
+  //   fetchComedyMovies: `/movie?api_key=${APIKEY}&with_genres=35`,
+  //   fetchHorrorMovies: `/movie?api_key=${APIKEY}&with_genres=27`,
+  //   fetchRomanceMovies: `/discover/romance?api_key=${APIKEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+  fetchUpcoming: `/movie/upcoming?api_key=${APIKEY}&language=en-US`,
+  fetchPopular: `/movie/popular?api_key=${APIKEY}&language=en-US`,
 };
 
 export default requests;
